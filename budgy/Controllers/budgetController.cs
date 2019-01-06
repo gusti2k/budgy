5e46a55bf4be2d58e18980f947cdae9986c15c78
gusti2k <frank.arnarsson@gmail.com>
2019-01-06 20:29:29 +0100

    Add project files.

---
+++ budgy/Controllers/budgetController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace budgy.Controllers
+{
+    public class budgetController : Controller
+    {
+
+        private budgy.Models.Budget b = new Models.Budget();
+        // GET: budget
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        private ActionResult Budget()
+        {
+            int resultat = 0;
+
+            resultat = b.Hus + b.Mad;
+
+
+            return null;
+        }
+    }
+}
---
+++ budgy/Models/Budget.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace budgy.Models
+{
+    public class Budget
+    {
+        public int BudgetId { get; set; }
+        public int Indkomst { get; set; }
+        public int Mad { get; set; }
+        public int Hus { get; set; }
+        
+    }
+}
